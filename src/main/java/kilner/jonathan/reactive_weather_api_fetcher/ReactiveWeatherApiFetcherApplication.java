faab2d4e5d2de4423fe6475541b5a5bd9eace9e4
Jonathan Kilner <jhow.k.132@gmail.com>
2026-06-06 21:02:08 -0300

    first commit

---
+++ src/main/java/kilner/jonathan/reactive_weather_api_fetcher/ReactiveWeatherApiFetcherApplication.java
@@ -0,0 +1,13 @@
+package kilner.jonathan.reactive_weather_api_fetcher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactiveWeatherApiFetcherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactiveWeatherApiFetcherApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kilner/jonathan/reactive_weather_api_fetcher/ReactiveWeatherApiFetcherApplicationTests.java
@@ -0,0 +1,13 @@
+package kilner.jonathan.reactive_weather_api_fetcher;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactiveWeatherApiFetcherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
